d47111a4778f532dac9c0ab47b9c0b405b79e0ee
Anna Luiza <marianoannaluiza@gmail.com>
2025-09-09 17:54:21 -0300

    feat: inicio crud categoria

---
+++ Frontend/src/app/lib/services/categoria/categoria.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { Categoria } from '@/app/shared/models/categoria.model';
+
+const LS_CHAVE = "categorias";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  constructor() { }
+
+  listarTodas() : Categoria[] {
+
+    const categorias = localStorage[LS_CHAVE];
+
+    return categorias ? JSON.parse(categorias) : [];     
+  } 
+
+  inserir(categoria: Categoria): void{
+
+    const categorias = this.listarTodas();
+
+    categoria.id = new Date().getTime();
+
+    categorias.push(categoria);
+
+   localStorage[LS_CHAVE] = JSON.stringify(categorias);
+  }
+
+  buscaPorId(id: number): Categoria | undefined {
+
+    const categorias = this.listarTodas();
+
+     return categorias.find(categoria => categoria.id === id);
+  }
+  
+  atualizar(categoria: Categoria): void {
+        
+    const categorias = this.listarTodas();
+
+    categorias.forEach((obj, index, objs) => {
+      if (categoria.id === obj.id) {
+         objs[index] = categoria
+      }
+  });
+
+  localStorage[LS_CHAVE] = JSON.stringify(categorias);
+}
+
+  remover(id: number): void {
+    let categorias = this.listarTodas();
+
+    categorias = categorias.filter(categoria => categoria.id !== id);
+
+    localStorage[LS_CHAVE] = JSON.stringify(categorias);
+  }
+
+}
