bb2f9624aafb2dfc8b136a0b26d2d3526b822029
Mateus-X <mateus.xavier@lactec.com.br>
2025-09-22 14:48:09 -0300

    fix/typing-for-order-related-screens

--- Frontend/src/app/lib/components/molecules/reject-card/reject-card.component.ts
+++ Frontend/src/app/lib/components/molecules/reject-card/reject-card.component.ts
@@ -1,9 +1,8 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
-import { ButtonComponent, TagComponent } from '@/app/lib/components';
-import { InputComponent } from '../input/input.component';
+import { ButtonComponent } from '@/app/lib/components';
 import {
   ReactiveFormsModule,
   FormBuilder,
@@ -12,14 +11,11 @@ import {
   Validators,
 } from '@angular/forms';
 import {
-  MatFormFieldControl,
   MatFormFieldModule,
 } from '@angular/material/form-field';
-import { Categoria } from '@/app/shared/models/categoria.model';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
-import { SelectInputComponent } from '../select-input/select-input.component';
 import { TextAreaInputComponent } from '../text-area-input/text-area-input.component';
 import { OrderRequest } from '@/app/@types';
 import { MatIcon } from '@angular/material/icon';
