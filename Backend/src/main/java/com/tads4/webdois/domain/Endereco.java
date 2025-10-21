11c9b1bd79af19d753fb9eac1c4cf319596ff3d6
Artur-Falavinha <artur.falavinha@gmail.com>
2025-10-20 23:45:37 -0300

    adiciona getters/setters nas classes Endereco e RegistroRequest e configura conexao com postgres

--- Backend/src/main/java/com/tads4/webdois/domain/Endereco.java
+++ Backend/src/main/java/com/tads4/webdois/domain/Endereco.java
@@ -15,6 +15,60 @@ public class Endereco implements Serializable {
     private String complemento;
     private String bairro;
     private String cidade;
-        
-    // preciso gerar getter e setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
 }
--- Backend/src/main/java/com/tads4/webdois/web/dto/RegistroRequest.java
+++ Backend/src/main/java/com/tads4/webdois/web/dto/RegistroRequest.java
@@ -10,5 +10,51 @@ public class RegistroRequest {
     private String perfil; 
     private Endereco endereco;
 
-    // preciso criar getter e setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 }
