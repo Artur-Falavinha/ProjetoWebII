13f6e6e3b6c1b5c3a3e98bb4c8e72b5a977db060
Artur-Falavinha <artur.falavinha@gmail.com>
2025-10-26 22:16:31 -0300

    Atualização do AuthService

--- Backend/src/main/java/com/tads4/webdois/infra/service/AuthService.java
+++ Backend/src/main/java/com/tads4/webdois/infra/service/AuthService.java
@@ -1,36 +1,32 @@
-package com.tads4.webdois.infra.service;
+// package com.tads4.webdois.infra.service;
 
-import com.tads4.webdois.domain.Usuario;
-import com.tads4.webdois.infra.repository.UsuarioRepository;
-import com.tads4.webdois.web.dto.RegistroRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
+// import com.tads4.webdois.domain.Usuario;
+// import com.tads4.webdois.infra.repository.UsuarioRepository;
+// import com.tads4.webdois.web.dto.RegistroRequest;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.stereotype.Service;
 
-import java.security.SecureRandom;
-import java.util.Random;
+// import java.security.SecureRandom;
+// import java.util.Random;
 
-@Service
-public class AuthService {
-    @Autowired
-    private UsuarioRepository usuarioRepository;
+// @Service
+// public class AuthService {
+//     @Autowired
+//     private UsuarioRepository usuarioRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+//     @Autowired
+//     private PasswordEncoder passwordEncoder;
 
-    /*@Autowired
-    private EmailService emailService;*/
+//     /*@Autowired
+//     private EmailService emailService;*/
 
-    public Usuario registrarUsuario(RegistroRequest registroRequest) {
-        if (usuarioRepository.existsByEmail(registroRequest.getEmail())) {
-            throw new RuntimeException("Erro: E-mail já está em uso!");
-        }
-        if (usuarioRepository.existsByCpf(registroRequest.getCpf())) {
-            throw new RuntimeException("Erro: CPF já está cadastrado!");
-        }
+//     public Usuario registrarUsuario(RegistroRequest registroRequest) {
+//         if (usuarioRepository.existsByEmail(registroRequest.getEmail())) {
+//             throw new RuntimeException("Erro: E-mail já está em uso!");
+//         }
+//         if (usuarioRepository.existsByCpf(registroRequest.getCpf())) {
+//             throw new RuntimeException("Erro: CPF já está cadastrado!");
+//         }
 
-        Usuario novoUsuario = new Usuario();
-
-
-    
-
+//         Usuario novoUsuario = new Usuario();
